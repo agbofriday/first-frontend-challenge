04b77c4fb55eeafa8cb45536b444535bab1e00f7
Friday A. <freewin@Fridays-MacBook-Air.local>
2024-03-08 11:06:01 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## frontend challenge
+
+- A responsive static landing page project.
+
+- Technologies used include HTML,CSS & JAVASCRIPT.
---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Close mobile naviagtion
+if (link.classList.contains("navigation-bar"))
+  headerEl.classList.toggle("nav-open");
